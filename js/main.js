ab47a4aa642ec113efc4f770eee987fb7084a8b8
200FI <alvaroandresespitiam@gmail.com>
2025-05-13 18:33:11 -0500

    Cambiado a 3 números

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ function sumar(a,b) {
 
 let numero1 = 10;
 let numero2 = 2;
+let numero3 = 7;
 
-let resultado = sumar(numero1, numero2);
+let resultado = sumar(numero1, numero2, numero3);
 
-console.log("El resultado de sumar " + numero1 + " y " + numero2 + " es: " + resultado);
+console.log("El resultado de sumar " + numero1 + " y " + numero2 + " y " + " es: " + resultado);
