7ee25bc276a241396fb65797aa6d0243ed59198e
200FI <alvaroandresespitiam@gmail.com>
2025-05-30 04:46:23 -0500

    Eliminacion de archivos y dejando html y css

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function sumar(a,b) {
+    return a + b;
+}
+
+let numero1 = 10;
+let numero2 = 2;
+let numero3 = 7;
+
+let resultado = sumar(numero1, numero2, numero3);
+
+console.log("El resultado de sumar " + numero1 + " y " + numero2 + " y " + " es: " + resultado);
